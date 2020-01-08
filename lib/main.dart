ad4027451ee138d4b139f8cb99d74737d983282b
Seme Mojugbe <sememojugbe@gmail.com>
2020-01-08 12:45:39 +0100

    Detail Page done. Moved to its own page Added google fonts and other assets

--- lib/main.dart
+++ lib/main.dart
@@ -11,83 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Detail Page Holder'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Stack(
-                children: <Widget>[
-                  FittedBox(
-                    fit: BoxFit.fill,
-                    child: Image.asset('assets/magaza/feed-pic-1.png'),
-                  ),
-                  Positioned(
-                    bottom: 30,
-                    left: 55,
-                    right: 20,
-                    child: Column(
-                      children: <Widget>[
-                        Text('Pohoda festival one of the best in Europe', style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 43,
-                            fontFamily: 'Yrsa',
-                            fontWeight: FontWeight.w600
-                        ),),
-                        SizedBox(height: 10,),
-                        Row(
-                          children: <Widget>[
-                            Text('0 Comments', style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.w600
-                            ),),
-                            SizedBox(width: 15,),
-                            Text('5 Likes', style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.w600
-                            ),),
-                            SizedBox(width: 15,),
-                            Text('26 Shares', style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.w600
-                            ),),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 25,
-                    child: Container(
-                      width: 1,
-                      height: 200,
-                      color: Color.fromRGBO(255, 255, 255, 0.5),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 215,
-                    left: 15,
-                    child: RotatedBox(
-                      quarterTurns: 3,
-                      child: Text('MUSIC', style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Montserrat',
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700
-                      ),),
-                    ),
-                  )
-                ],
-              )
-            ],
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: DetailScreen(),
           ),
         )
       ),
