852978a6459f4a0d48066e9fd275607aa7d494c5
Seme Mojugbe <sememojugbe@gmail.com>
2020-02-25 12:14:24 +0100

    Intro Slider now working with Next/Prev buttons.

--- lib/magaza_ui/screens/intro_slider.dart
+++ lib/magaza_ui/screens/intro_slider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:transformer_page_view/transformer_page_view.dart';
 import 'package:magaza_flutter/magaza_ui/screens/slide_meta.dart';
+import 'package:transformer_page_view/transformer_page_view.dart' as prefix0;
 
 
 class IntroSliderScreen extends StatefulWidget {
@@ -9,7 +10,15 @@ class IntroSliderScreen extends StatefulWidget {
 }
 
 class _IntroSliderScreenState extends State<IntroSliderScreen> {
+  String nextText = 'Next';
+  String prevText = 'Prev';
 
+  onDone(int index){
+    print('index: '+index.toString()+', pageSlides.lenght is'+pageSlides.length.toString());
+    if(index == (pageSlides.length-1)){
+      Navigator.pushNamed(context, '/');
+    }
+  }
   List<SlideMeta> pageSlides = [
     SlideMeta(
         title: 'First Title',
@@ -33,12 +42,20 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
         backgroundImage: 'assets/magaza/slider3.png'
     ),
   ];
+
+  TransformerPageController slider;
+
   @override
+  void initState(){
+    super.initState();
+    // set the itemCount to lenght of pageSlide Array
+    slider = TransformerPageController( itemCount: pageSlides.length);
+  }
 
   Widget build(BuildContext context) {
-
     return new TransformerPageView(
         loop: true,
+        pageController: slider,
         itemBuilder: (BuildContext context, int index) {
           return Container(
               color: pageSlides[index].backgroundColor,
@@ -86,6 +103,39 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
                       color: Colors.white,
                     ),
                   ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: FlatButton(
+                            child: Text('Prev'),
+                            onPressed: (){
+                              setState(() {
+                                slider.previousPage(duration: Duration(seconds: 1), curve: Cubic(0.25, 0.1, 0.25, 1.0));
+                              });
+                            },
+                          ),
+                          flex: 1,
+                        ),
+                        Spacer(),
+                        Spacer(),
+                        Expanded(
+                          child: FlatButton(
+                            child: Text('Next'),
+                            onPressed: (){
+                              setState(() {
+                                slider.nextPage(duration: Duration(seconds: 1), curve: Cubic(0.25, 0.1, 0.25, 1.0));
+                              });
+                            },
+                          ),
+                          flex: 1,
+                        ),
+                      ],
+                    )
+                  )
                 ],
               )
           );
