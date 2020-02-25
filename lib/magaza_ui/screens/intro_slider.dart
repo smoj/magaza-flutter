fe7d0a2a9c236ae65325369d66d652990854770f
Seme Mojugbe <sememojugbe@gmail.com>
2020-02-25 13:12:19 +0100

    UI Misc changes. Added changewording fxn

--- lib/magaza_ui/screens/intro_slider.dart
+++ lib/magaza_ui/screens/intro_slider.dart
@@ -20,25 +20,34 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
     }
   }
 
+  changeWording(int index){
+    print('index: '+index.toString()+', pageSlides.lenght is'+pageSlides.length.toString());
+    if(index == (pageSlides.length-1)){
+      setState(() {
+        nextText = 'Done';
+      });
+    }
+  }
+
 //  Change/Add as many slides are you need
   List<SlideMeta> pageSlides = [
     SlideMeta(
-        title: 'First Title',
-        message: 'Second message',
+        title: 'Welcome to Magaza',
+        message: 'A news app for iOS & Android built entirely in flutter',
         backgroundColor: Colors.white,
         textColor: Colors.black,
         backgroundImage: 'assets/magaza/slider1.png'
     ),
     SlideMeta(
-        title: 'Second Title',
-        message: 'Second message',
+        title: 'Beautiful custom screens',
+        message: 'Based on a custom UI Kit for news apps',
         backgroundColor: Colors.white,
         textColor: Colors.black,
         backgroundImage: 'assets/magaza/slider2.png'
     ),
     SlideMeta(
-        title: 'Third Title',
-        message: 'Third message',
+        title: 'Thoroughly Tested. Up to date',
+        message: 'By the FlutterMarket, across iOS/Android devices on flutter 1.12.x',
         backgroundColor: Colors.white,
         textColor: Colors.black,
         backgroundImage: 'assets/magaza/slider3.png'
@@ -66,24 +75,30 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
                   Positioned(
                       top: 464,
                       left: 32,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(pageSlides[index].title, style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'Yrsa',
-                              fontWeight: FontWeight.w600,
-                              fontSize: 36,
-                              decoration: TextDecoration.none
-                          ),),
-                          Text(pageSlides[index].message, style: TextStyle(
-                              color: pageSlides[index].textColor,
-                              fontSize: 20,
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.w300,
-                              decoration: TextDecoration.none
-                          ),)
-                        ],
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*0.9,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(height: 10,),
+                            Text(pageSlides[index].title, style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Yrsa',
+                                fontWeight: FontWeight.w600,
+                                fontSize: 36,
+                                height: 0.8,
+                                decoration: TextDecoration.none
+                            ),),
+                            SizedBox(height: 12,),
+                            Text(pageSlides[index].message, style: TextStyle(
+                                color: pageSlides[index].textColor,
+                                fontSize: 20,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w300,
+                                decoration: TextDecoration.none
+                            ),)
+                          ],
+                        ),
                       )
                   ),
                   Container(
@@ -97,12 +112,12 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
                     width: MediaQuery.of(context).size.width,
                   ),
                   Positioned(
-                    top: 368,
-                    left: 32,
+                    top: 358,
+                    left: 18,
                     child: Container(
-                      width: 64,
-                      height: 64,
-                      color: Colors.white,
+                      width: 96,
+                      height: 96,
+                      child: Image.asset('assets/magaza/logo.png', fit: BoxFit.fitWidth,),
                     ),
                   ),
                   Positioned(
@@ -113,7 +128,7 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
                       children: <Widget>[
                         Expanded(
                           child: FlatButton(
-                            child: Text('Prev'),
+                            child: Text("$prevText"),
                             onPressed: (){
                               setState(() {
                                 slider.previousPage(duration: Duration(seconds: 1), curve: Cubic(0.25, 0.1, 0.25, 1.0));
@@ -126,7 +141,7 @@ class _IntroSliderScreenState extends State<IntroSliderScreen> {
                         Spacer(),
                         Expanded(
                           child: FlatButton(
-                            child: Text('Next'),
+                            child: Text("$nextText"),
                             onPressed: (){
                               setState(() {
                                 slider.nextPage(duration: Duration(seconds: 1), curve: Cubic(0.25, 0.1, 0.25, 1.0));
